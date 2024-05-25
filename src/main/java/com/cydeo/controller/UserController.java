f64715df46f8bcae84e57ed60e800ab0f936df66
Yasin K. <yasinufukkanlioglu@gmail.com>
2024-05-25 11:02:16 +0100

    SMApp

--- src/main/java/com/cydeo/controller/UserController.java
+++ src/main/java/com/cydeo/controller/UserController.java
@@ -1,4 +1,70 @@
 package com.cydeo.controller;
 
+import com.cydeo.DTO.UserDTO;
+import com.cydeo.service.AddressService;
+import com.cydeo.service.RoleService;
+import com.cydeo.service.UserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/user")
 public class UserController {
+    private final UserService userService;
+    private final RoleService roleService;
+    private final AddressService addressService;
+
+    public UserController(UserService userService, RoleService roleService, AddressService addressService) {
+        this.userService = userService;
+        this.roleService = roleService;
+        this.addressService = addressService;
+    }
+
+    @GetMapping("/create")
+    public String createUser(Model model) {
+
+        model.addAttribute("user", new UserDTO());
+        model.addAttribute("roles", roleService.findAll());
+        model.addAttribute("addresses", addressService.findAll());
+        model.addAttribute("states", addressService.findAllStates());
+        model.addAttribute("users", userService.findAll());
+
+        return "user/user-create";
+    }
+
+    @PostMapping("/create")
+    public String createUser(@ModelAttribute("user") UserDTO userDTO) {
+        userService.save(userDTO);
+
+        return "redirect:/user/create";
+    }
+
+    @GetMapping("/update/{username}")
+    public String updateUser(@PathVariable ("username") String username, Model model) {
+        model.addAttribute("user", userService.findById(username));
+        model.addAttribute("roles", roleService.findAll());
+        model.addAttribute("addresses", addressService.findAll());
+        model.addAttribute("states", addressService.findAllStates());
+        model.addAttribute("users", userService.findAll());
+
+        return "user/user-update";
+    }
+
+    @PostMapping("/update")
+    public String updateUser(@ModelAttribute("user") UserDTO user) {
+
+        userService.update(user);
+
+        return "redirect:/user/create";
+    }
+
+    @GetMapping("/delete/{username}")
+    public String deleteUser(@PathVariable ("username") String username) {
+
+        userService.deleteById(username);
+
+        return "redirect:/user/create";
+    }
+
 }
--- src/main/java/com/cydeo/converter/UserConverter.java
+++ src/main/java/com/cydeo/converter/UserConverter.java
@@ -1,4 +1,21 @@
 package com.cydeo.converter;
 
-public class UserConverter {
+
+import com.cydeo.DTO.UserDTO;
+import com.cydeo.service.UserService;
+import org.springframework.boot.context.properties.ConfigurationPropertiesBinding;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationPropertiesBinding
+public class UserConverter implements Converter<String, UserDTO> {
+    UserService userService;
+    public UserConverter(UserService userService) {
+        this.userService = userService;
+    }
+
+    public UserDTO convert(String source) {
+        return userService.findById(source);
+    }
 }
